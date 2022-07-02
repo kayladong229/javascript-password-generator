2e807b69e0ee934b195833262f7e1000d63da0ad
kayladong229 <105991864+kayladong229@users.noreply.github.com>
2022-07-01 22:45:54 -0700

    Added generator code to script.js

--- script.js
+++ script.js
@@ -1,6 +1,94 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//Declares main generatePassword function
+function generatePassword () {
+//Declares all possible password characters through arrays
+  var lowercaseChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var uppercaseChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var specialChar = [' ', '&', '@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.', '>', '<'];
+  //Created an array that will house the user's chosen character type arrays
+  var chosenChars = [];
+  //Asks the user to select an appropriate password length
+  passwordLength = prompt("Pick a number between 8 and 128. This determines the length of your password.");
+    if (passwordLength >= 8 && passwordLength <= 128) {
+      window.alert("You will now be presented a series of prompts regarding your preferred password criteria starting with lowercase letters.");
+      askLowercase(); 
+    } else {
+      //The user starts the password generating process over if unfavorable password length is picked
+      window.alert("Please select a number between 8 and 128.");
+      generatePassword();
+    }
+
+    //Declares sub-functions for all character types
+
+  function askLowercase () {
+    //Asks the user to say yes or no by clicking either OK or Cancel respectively
+    var answerLowercase = confirm("Do you wish to include lowercase letters in your password? (Y/N)");
+    if (answerLowercase) {
+      //Concatenates chosenChars array with appropriate character type array if user clicks OK
+      chosenChars = chosenChars.concat(lowercaseChar);
+      //Also introduces next criteria option   
+      window.alert("Next, uppercase letters.");
+      //And executes function regarding that next option
+      askUppercase();
+    } else {
+      //Skips concatenation if user clicks "Cancel"
+      window.alert("Next, uppercase letters.");
+      askUppercase();
+    }
+  } 
+
+  function askUppercase () {
+    var answerUppercase = confirm("Do you wish to include uppercase letters in your password? (Y/N)");
+    if (answerUppercase) {
+        chosenChars = chosenChars.concat(uppercaseChar);
+        window.alert("Next, numbers.");
+        askNumbers();
+      } else {
+        window.alert("Next, numbers.");
+        askNumbers();
+      }
+    }
+
+    function askNumbers () {
+      var answerNumbers = confirm("Do you wish to include numbers in your password? (Y/N)");
+      if (answerNumbers) {
+        chosenChars = chosenChars.concat(numbers);
+        window.alert("Last, special characters.");
+        askSpecial();
+      } else {
+        window.alert("Last, special characters.");
+        askSpecial();
+      }
+    }
+
+    function askSpecial () {
+      var answerSpecial = confirm("Do you wish to include special characters in your password? (Y/N)");
+      if (answerSpecial) {
+        chosenChars = chosenChars.concat(specialChar);
+        //This is the last criteria option so the user is informed their password is ready after making their choice
+        window.alert("Password generated.");
+      } else {
+        window.alert("Password generated.");
+      }
+    }
+
+  // if (!confirmLowercase && !confirmUppercase && !confirmNumbers && !confirmSpecial) {
+  //   window.alert("You are required to choose at least one character type.");
+  //   generatePassword();
+  // }
+
+    //Declares final password outcome that will show up in the #password input
+    var passwordFinal = "";
+    for (var i = 0; i < passwordLength; i++) {
+      //Created a variable that selects random characters out of every type the user chose  
+      charMix = [Math.floor(Math.random() * chosenChars.length)];
+      passwordFinal = passwordFinal + chosenChars[charMix];
+    }
+   return passwordFinal;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -9,6 +97,7 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
